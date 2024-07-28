a42dee794b92030b3a19663a4faa5c661ac9956b
村中信隆 <chanayataka2324@gmail.com>
2024-07-28 17:29:57 +0900

    Post.tsxのコンポーネントファイルを作成しApp.tsxから引越し

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import Blog from "./components/Blog";
 import Header from "./components/Header";
+import Post from "./components/Post";
 import Footer from "./components/Footer";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  const fetchPosts = () => {
-    // Using axios to fetch the posts
-    axios.get("http://suzukazine.local/wp-json/wp/v2/posts").then((res) => {
-      // Saving the data to state
-      setPosts(res.data);
-    });
-  };
-
-  // Calling the function on page load
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-
   return (
     <>
       <Header />
-      <div>
-        {posts.map((item) => (
-          <Blog post={item} />
-        ))}
-      </div>
+      <Post />
       <Footer />
     </>
   );
