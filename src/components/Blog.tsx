caf639b33514987e61e33124869c25d5e8da3ade
村中信隆 <chanayataka2324@gmail.com>
2024-07-28 17:00:46 +0900

    first commit

---
+++ src/components/Blog.tsx
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./Blog.css";
+
+interface Post {
+  title: {
+    rendered: string;
+  };
+  excerpt: {
+    rendered: string;
+  };
+  _links: {
+    ["wp:featuredmedia"]: [
+      {
+        href: string;
+      }
+    ];
+  };
+}
+
+interface BlogProps {
+  post: Post;
+}
+
+export default function Blog({ post }: BlogProps) {
+  const [featuredImage, setFeaturedimage] = useState<string | undefined>();
+
+  const getImage = () => {
+    axios.get(post?._links["wp:featuredmedia"][0]?.href).then((response) => {
+      setFeaturedimage(response.data.source_url);
+    });
+  };
+
+  useEffect(() => {
+    getImage();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="blog-container">
+        <p className="blog-date">
+          {new Date(Date.now()).toLocaleDateString("en-US", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          })}
+        </p>
+        <h2 className="blog-title">{post.title.rendered}</h2>
+        <p
+          className="blog-excerpt"
+          dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+        />
+        <img src={featuredImage} className="mask" />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <p className="text-center m-5 text-3xl font-bold">SUZUKAZINE</p>
+      <div className="bg-black w-full">
+        <ul className="flex text-white p-5 gap-12 place-content-center">
+          <li>グルメ</li>
+          <li>生活</li>
+          <li>観光</li>
+          <li>お問い合わせ</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Header;
