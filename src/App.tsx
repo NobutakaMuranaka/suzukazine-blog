bf0ff88e520e990ffec8ceddbcde9169049a0684
村中信隆 <chanayataka2324@gmail.com>
2024-08-02 00:08:36 +0900

    遷移後のページを最上部から表示する

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Post from "./components/Post";
@@ -9,17 +15,28 @@ import Profile from "./components/Profile";
 import Category from "./components/Category";
 import NotFound from "./components/404";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
       <Header />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Post />} />
         <Route path="/post/:id" element={<PostDetail />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/category/:categoryName" element={<Category />} />{" "}
+        <Route path="/category/:categoryName" element={<Category />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
