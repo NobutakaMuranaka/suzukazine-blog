bee762b8ef7bd06d38d10f6b28bfa8985f0f7235
村中信隆 <chanayataka2324@gmail.com>
2024-08-03 16:47:38 +0900

    react-shareでSNSシェアボタンを追加

---
+++ src/components/SnsShare.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  FacebookIcon,
+  TwitterIcon,
+} from "react-share";
+
+const iconsize = 32;
+const round = true;
+
+const SnsShare = () => {
+  return (
+    <>
+      <div className="gap-5 flex m-auto justify-center mb-5">
+        <div>
+          <FacebookShareButton url={"https://www.suzukazine.com/"}>
+            <FacebookIcon size={iconsize} round={round} />
+          </FacebookShareButton>
+        </div>
+        <div>
+          <TwitterShareButton url={"https://www.suzukazine.com/"}>
+            <TwitterIcon size={iconsize} round={round} />
+          </TwitterShareButton>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SnsShare;
