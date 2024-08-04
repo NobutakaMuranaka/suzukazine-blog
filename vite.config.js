95c44f1e0d2e6b4d151c4f4e852ae5dbdba4a987
村中信隆 <chanayataka2324@gmail.com>
2024-08-04 14:29:04 +0900

    githubアクション用コードを削除

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import * as path from "path";
+export default defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    base: "/",
+    build: {
+        outDir: path.resolve(process.cwd(), "src/dist"), // 出力ディレクトリをsrc/distに設定
+        assetsDir: "assets", // アセットディレクトリを設定
+        rollupOptions: {
+            input: {
+                main: path.resolve(process.cwd(), "index.html"), // エントリーポイントを設定
+            },
+            output: {
+                entryFileNames: "assets/[name].js",
+                chunkFileNames: "assets/[name].js",
+                assetFileNames: "assets/[name].[ext]",
+            },
+        },
+    },
+    server: {
+        watch: {
+            usePolling: true, // ファイルの変更をポーリングで監視
+        },
+        open: true, // ローカルサーバー起動時にブラウザを自動で開く
+    },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
-/// <reference types="vitest"/>
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import * as path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: "happy-dom",
-    setupFiles: ["./vitest-setup.ts"],
+  base: "/",
+  build: {
+    outDir: path.resolve(process.cwd(), "src/dist"), // 出力ディレクトリをsrc/distに設定
+    assetsDir: "assets", // アセットディレクトリを設定
+    rollupOptions: {
+      input: {
+        main: path.resolve(process.cwd(), "index.html"), // エントリーポイントを設定
+      },
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true, // ファイルの変更をポーリングで監視
+    },
+    open: true, // ローカルサーバー起動時にブラウザを自動で開く
   },
 });
